f5caba71012df5f4f427d5ba0374b28d9edfc6c1
SheebaHashmi <sheebahashmi0@gmail.com>
2022-01-21 13:53:58 -0500

    Create database

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require('express')
+const resourceRouter= require('../api/resource/router');
+const projectRouter= require('../api/project/router');
+const taskRouter= require('../api/task/router');
+
+const server = express();
+
+server.use(express.json())
+server.use('/api/resources',resourceRouter)
+server.use('/api/projects',projectRouter)
+server.use('/api/tasks',taskRouter)
+
+server.use((err,req,res,next) => {
+    res.status(500).json({
+        message:err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = server
---
+++ data/migrations/20220121165710_projects.js
@@ -0,0 +1,55 @@
+
+exports.up = async function(knex) {
+    await knex.schema
+          .createTable('projects',table => {
+              table.increments("project_id")
+              table.string("project_name",128).notNullable()
+              table.string("project_description",256)
+              table.boolean("project_completed").defaultTo(false)
+          })
+          .createTable('resources',table => {
+              table.increments("resource_id")
+              table.string("resource_name",128).notNullable()
+              table.text("resource_description")
+          })
+          .createTable('tasks',table => {
+              table.increments("task_id")
+              table.string("task_description").notNullable()
+              table.string("task_notes")
+              table.boolean("task_completed").defaultTo(false)
+              table.integer('project_id')
+                  .unsigned()
+                  .notNullable()
+                  .references('project_id')
+                  .inTable('projects')
+                  .onDelete('CASCADE')
+                  .onUpdate('CASCADE')
+  
+          })
+          .createTable('project_resources',table => {
+              table.increments("project_resource_id")
+              table.integer('project_id')
+                  .unsigned()
+                  .notNullable()
+                  .references('project_id')
+                  .inTable('projects')
+                  .onDelete('CASCADE')
+                  .onUpdate('CASCADE')
+              table.integer('resource_id')
+                  .unsigned()
+                  .notNullable()
+                  .references('resource_id')
+                  .inTable('resources')
+                  .onDelete('CASCADE')
+                  .onUpdate('CASCADE')
+          })
+  };
+  
+  exports.down = async function(knex) {
+    await knex.schema
+      .dropTableIfExists('projects')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('project_resources')
+  };
+  
