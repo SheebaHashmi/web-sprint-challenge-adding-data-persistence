a0394bf49a8cd32e57aa879eb0e8ad58416f5439
SheebaHashmi <sheebahashmi0@gmail.com>
2022-01-21 14:09:00 -0500

    Fix knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,31 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './projects.db3'
+      filename:'./projects.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      }
     }
+    
+  },
+  staging: {
+  
   },
 
-  migrations: {
-    directory: './data/migrations'
-  },
-  seeds: {
-    directory: './data/seeds'
-  },
-  pool: {
-    afterCreate: (conn, done) => {
-      // runs after a connection is made to the sqlite engine
-      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-    },
-  },
-};
+  production: {
+   
+  }
+}
